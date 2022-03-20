6ac13da0bd11f76415bf358bf918226ef84a90e5
Jjacex <jacexecaj@gmail.com>
2022-03-20 06:59:24 -0500

    finished backend api with authentication. time to break this down and rewrite it

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 const asyncHandler = require('express-async-handler')
 
 // @desc gets goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    //because req.user is specified in the authMiddleware, the user is now avaiable in the request object
+    //we can then look up the user by the id
+    const goals = await Goal.find({user: req.user.id}) //find all the goals which belong to a specific id
     res.status(200).json(goals)
 })
 
@@ -18,7 +21,11 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        //the model for this collection requires a user
+        //the user is claimed from the authMiddleware ran directly before this controller function
+        //we now grab that user from the request obbject and we use the user id to set the user for the goal
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -32,6 +39,23 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    //grabbing our user from the request object.
+    //the user is here because our auth middleware pulled it in directly before thie function call
+    const user = await User.findById(req.user.id)
+
+    //checking for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
     res.status(200).json(updateGoal)
 })
@@ -45,6 +69,23 @@ const deleteGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    //grabbing our user from the request object.
+    //the user is here because our auth middleware pulled it in directly before thie function call
+    const user = await User.findById(req.user.id)
+
+    //checking for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     await goal.remove()
     res.status(200).json({id: req.params.id})
 })
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,3 +1,38 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
 
+const protect = asyncHandler(async (req, res, next) => {
+    let token
+
+    //tokens start with Bearer, so we can check to see if this is included in the req.headers.authorization
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            //get token from header
+            //we split this into an array and 0 will contain "Bearer" and 1 will cointain the token
+            token = req.headers.authorization.split(' ')[1]
+
+            //verify the token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            //get user from the token
+            //this is now available in any controller function ran after this piece of middleware
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.log(error)
+            res.status(401)
+            throw new Error('Not authorized')
+        }
+    }
+
+    if(!token){
+        res.status(401)
+        throw new Error('Not authorize, no token')
+    }
+})
+
+module.exports = {
+    protect
+}
