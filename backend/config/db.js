92b35d0677a38e37c5a11933e168295de770f05d
Jjacex <jacexecaj@gmail.com>
2022-03-22 04:21:54 -0500

    starting on front end of application

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,3 +11,14 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB
+
+//notes on this file after review
+//I understand this file very well and I could rewrite it without much help
+//the most challenging aspect is pulling in your mongouri from your .env file
+//I have learned you must keep your .env in the root of your entire project
+//this helps ensure you have access to it from the entire project
+//mongoose is used here and it creates a simpler way to ensure mongodb is connected
+//the biggest challenge I see here is authentication
+//I know some mongoose functions are not processed through the validation you set in the model
+//dealing with this and testing for this can be a challenge
+//other than that, I feel confident here and could recreate this connection process with minimal difficulty
